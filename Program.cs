60daf7e4ca3589e344e8ecbb127c05c0809216c8
yujichino <chino@jsl.co.jp>
2020-03-17 20:16:54 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+
+class PipeServerText
+{
+    static void Main()
+    {
+        using (NamedPipeServerStream pipe = new NamedPipeServerStream("/tmp/app.testpipe"))
+        {
+            Console.WriteLine("NamedPipeServerStream object created.");
+
+            // Wait for a client to connect
+            Console.Write("Waiting for client connection...");
+            pipe.WaitForConnection();
+
+            Console.WriteLine("Client connected.");
+            try
+            {
+                Console.Write("Enter text: ");
+                var str = Console.ReadLine();
+                Console.WriteLine(str);
+
+                var writer = new StreamWriter(pipe);
+
+                writer.Write("{ \"type\": \"app.message\", \"data\": \"" + str + "\" }" + "\f");
+                writer.Flush();
+                // pipe.WaitForPipeDrain();
+
+            }
+            // Catch the IOException that is raised if the pipe is broken
+            // or disconnected.
+            catch (IOException e)
+            {
+                Console.WriteLine("ERROR: {0}", e.Message);
+            }
+        }
+    }
+}
